92266ba5e4ae7a6985dcae8e13b86a64ebd405d6
Amey Kadwe <143980556+Ameykadwe19@users.noreply.github.com>
2025-06-04 14:53:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,13 +14,6 @@ app.use('/api/chargers', require('./routes/chargerRoutes'));
 
 
 const PORT = process.env.PORT || 5000;
-
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
-
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
   await sequelize.sync(); // Sync DB
