20e9b7ec4f3d611673781e2079caedcec36658d1
Amey Kadwe <143980556+Ameykadwe19@users.noreply.github.com>
2025-06-14 02:45:57 +0530

    Update useAuth.js

--- frontend/src/composables/useAuth.js
+++ frontend/src/composables/useAuth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import jwt_decode from 'jwt-decode' // needed for decoding
+import { default as jwt_decode } from 'jwt-decode'
 
 const API_URL = import.meta.env.VITE_API_URL
 
