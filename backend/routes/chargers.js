d32aa610174659d5747e750f40cd62e99917f38b
Amey Kadwe <143980556+Ameykadwe19@users.noreply.github.com>
2025-06-13 18:17:36 +0530

    Update chargers.js

--- backend/routes/chargers.js
+++ backend/routes/chargers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const isAdmin = require('../middleware/isAdmin'); // <-- Add this line
 const {
   getChargers,
   getCharger,
