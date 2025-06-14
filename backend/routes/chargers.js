50c55ed391ede1fa87211421b969e0647dc42028
Amey Kadwe <143980556+Ameykadwe19@users.noreply.github.com>
2025-06-14 18:30:02 +0530

    Update chargers.js

--- backend/routes/chargers.js
+++ backend/routes/chargers.js
@@ -1,14 +1,24 @@
-const express = require('express'); 
+const express = require('express');
 const { protect, authorize } = require('../middleware/auth');
+const {
+  getChargers,
+  getCharger,
+  createCharger,
+  updateCharger,
+  deleteCharger
+} = require('../controllers/chargerController'); 
+
 const router = express.Router();
-router.use(protect);  // All routes need login
+
+router.use(protect); // All routes require login
 
 router.route('/')
-  .get(getChargers)
-  .post(createCharger); // All users can create
+  .get(getChargers)         
+  .post(createCharger);     
 
 router.route('/:id')
-  .get(getCharger)
-  .put(updateCharger)
-  .delete(deleteCharger); // We’ll restrict in controller (based on role + ownership)
+  .get(getCharger)         
+  .put(updateCharger)      
+  .delete(deleteCharger);   
 
+module.exports = router; 
